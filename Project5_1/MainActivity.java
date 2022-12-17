23783dad5d27d748beeaf6724854529b9d78a4d8
ssongK <73736882+ssongK@users.noreply.github.com>
2022-12-17 16:56:28 +0900

    221217

--- Project5_1/MainActivity.java
+++ Project5_1/MainActivity.java
@@ -14,23 +14,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
+        //setContentView(R.layout.activity_main);  // activity_main.xml을 삭제해서 주석처리
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams( // 리니어레이아웃 파라미터 설정
+                LinearLayout.LayoutParams.MATCH_PARENT, // android:layout_width="match_parent"
+                LinearLayout.LayoutParams.MATCH_PARENT); // android:layout_height="match_parent"
+                // 하나 더 추가해 android:layout_height="1" 지정 가능
 
-        LinearLayout baseLayout = new LinearLayout(this);
-        baseLayout.setOrientation(LinearLayout.VERTICAL);
-        baseLayout.setBackgroundColor(Color.rgb(0,255,0));
-        setContentView(baseLayout,params);
+        LinearLayout baseLayout = new LinearLayout(this); // 리니어레이웃 생성
+        baseLayout.setOrientation(LinearLayout.VERTICAL); // android:orientation="vertical"
+        baseLayout.setBackgroundColor(Color.rgb(0,255,0)); // android:background="#00FF00"
+        setContentView(baseLayout,params); // 준비된 리니어레이아웃 변수를 화면에 출력
 
-        Button btn = new Button(this);
-        btn.setText("버튼입니다");
-        btn.setBackgroundColor(Color.MAGENTA);
-        baseLayout.addView(btn);
+        Button btn = new Button(this); // 버튼 생성
+        btn.setText("버튼입니다"); // android:text="버튼입니다"
+        btn.setBackgroundColor(Color.MAGENTA); // android:background="#magenta"
+        baseLayout.addView(btn); // 레이아웃에 출력(android:layout_width="match_parent", android:layout_height="wrap_content")
 
         btn.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View arg0){
+            public void onClick(View arg0){
                 Toast.makeText(getApplicationContext(),"코드로 생성한 버튼입니다.",Toast.LENGTH_SHORT).show();
             }
         });
